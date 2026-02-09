03e8beabcad7d6d81511e20e4317a4138237e096
Amber Mcguire <growthpainz@protonmail.com>
2026-02-09 17:04:06 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -651,7 +651,7 @@ export default function Layout({ children, currentPageName }) {
               </NavLink>
 
               <div className="pt-4">
-                <p className="px-4 pt-2 pb-1 text-xs font-semibold text-blue-400 uppercase tracking-wider">🔮 Reading</p>
+                <p className="px-4 pt-2 pb-1 text-xs font-semibold text-blue-400 uppercase tracking-wider">Reading</p>
                 {readingLinks.map(link => (
                   <NavLink key={link.href} to={createPageUrl(link.href)}>
                     <link.icon className="w-5 h-5 mr-3" />
